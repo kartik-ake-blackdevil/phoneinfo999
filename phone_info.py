7f3493023a74ff85539077efb1177de403bd4844
kartik-ake-blackdevil <109588020+kartik-ake-blackdevil@users.noreply.github.com>
2022-07-19 15:01:41 +0530

    Add files via upload

---
+++ phone_info.py
@@ -0,0 +1,14 @@
+import requests
+
+phone_no =input("Enter the number: ")
+
+Data={"use":phone_no,"pass":"","ciso":"IN","IP":"49.36.237.230","IP_COUNTRY":"India","IPADDRESS":"49.36.237.230","GEOIP_COUNTRY_ISO":"IN","originalreferer":"https://m.indiamart.com/my/?ref=/","ph_code":"91","glusr_usr_ip":"49.36.237.230","duplicateEmailCheck":""}
+
+r = requests.post('https://m.indiamart.com/ajaxrequest/identified/common/login', json=Data)
+print()
+print("printing status code: ", r.status_code)
+print()
+print("now we are loadind info of the user ")
+print()
+print(r.json())
+
